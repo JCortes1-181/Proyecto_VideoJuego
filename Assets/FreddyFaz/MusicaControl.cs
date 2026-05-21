3b1e6793d8d54fb006307948e22ce12b5d83cd66
JCortes1-181 <joanpaziia@gmail.com>
2026-05-21 00:20:16 -0400

    minijuego skate arreglado

--- Assets/FreddyFaz/MusicaControl.cs
+++ Assets/FreddyFaz/MusicaControl.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 
-public class Musicacontrol : MonoBehaviour
+public class musicacontrol : MonoBehaviour
 {
-    // La variable 'instancia' ahora coincide con el nombre de la clase
-     public static Musicacontrol instancia;
+    public static musicacontrol instancia; // Corregido el tipo a minúsculas
     private AudioSource miAudio;
 
     void Awake()
     {
-        // Sistema Singleton: evita que la música se reinicie al cambiar de escena
         if (instancia == null)
         {
             instancia = this;
@@ -17,26 +15,15 @@ public class Musicacontrol : MonoBehaviour
         }
         else
         {
-            // Si ya existe un gestor de música, destruye el nuevo para que no se solapen
             Destroy(gameObject);
         }
     }
 
-    // Esta es la función que llamamos desde JuegoGeneral
     public void ReproducirMusica()
     {
         if (miAudio != null && !miAudio.isPlaying)
         {
             miAudio.Play();
-        }
-    }
-
-    // Esta es la función que llamamos desde ControladorVidas al perder
-    public void DetenerMusica()
-    {
-        if (miAudio != null)
-        {
-            miAudio.Stop();
         }
     }
 }
---
+++ Assets/Scripts/Minijuego_Spaceinvader/GameManager.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //[SerializeField]
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
